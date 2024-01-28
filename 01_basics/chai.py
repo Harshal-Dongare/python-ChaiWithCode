f78652b046e9f1cb764645a28615f9d581615ec8
Harshal-Dongare <myeducationalarena@gmail.com>
2024-01-28 20:54:50 +0530

    Print hello in python

---
+++ 01_basics/chai.py
@@ -0,0 +1,4 @@
+# use chai() function from different file
+from hello_python import chai
+
+chai("This is chai from other function")
---
+++ 01_basics/hello_python.py
@@ -0,0 +1,12 @@
+# Prints the output
+print("chai aur python")
+
+# -----------------------------------------------------------------------------------
+
+# functions in python
+def chai(n):
+    print(n)
+
+# function call
+chai("Lemon tea")
+chai(4)
